dedae903a4f6f6faaaca34d7c1dbeb63f21a239b
vassilis <vas.terzo@gmail.com>
2015-04-01 10:36:58 +0300

    added opacity and width modifiers

--- grid.js
+++ grid.js
@@ -3,7 +3,7 @@
     var done, initGrrrridlet, script, v;
     initGrrrridlet = function() {
       return (window.grrrridlet = function() {
-        var el, $body, $col, $container, $grid, $row, i, class_list;
+        var el, $body, $style, $grrrrid, $grrrrid_container, class_list, opacity, viewport, width, initial_width;
           el = document;
           $body = el.body;
           if ($body.querySelector(".grrrrid") !== null) {
@@ -21,11 +21,46 @@
             $body.appendChild($style);
             $body.appendChild($grid);
 
-            return el.addEventListener("keyup", function(e) {
+            $grrrrid = el.querySelector(".grrrrid");
+            $grrrrid_container = el.querySelector(".grrrrid-container");
+            initial_width = parseFloat(getComputedStyle($grrrrid_container)["width"]);
+
+            return el.addEventListener("keydown", function(e) {
+              // console.log(e.keyCode);
+              opacity = parseFloat(getComputedStyle($grrrrid)["opacity"]);
+              viewport = parseFloat($body.clientWidth);
+              width = parseFloat(getComputedStyle($grrrrid_container)["width"]);
+
+              if (e.keyCode === 49 && e.shiftKey || e.keyCode === 35) { // reset
+                $grrrrid_container.style.width = initial_width + "px";
+              }
+              if (e.keyCode === 37 && e.shiftKey) { // left
+                if (width > 0) {
+                  $grrrrid_container.style.width = width - 1 + "px";
+                }
+              }
+              if (e.keyCode === 39 && e.shiftKey) { // right
+                if (width < viewport) {
+                  $grrrrid_container.style.width = width + 1 + "px";
+                }
+              }
+              if (e.keyCode === 38 && e.shiftKey) { // up
+                if (opacity < 1) {
+                  $grrrrid.style.opacity = opacity + 0.1;
+                }
+                return false;
+              }
+              if (e.keyCode === 40 && e.shiftKey) { // down
+                if (opacity > 0) {
+                  $grrrrid.style.opacity = opacity - 0.1;
+                }
+                return false;
+              }
               if (e.keyCode === 70 && e.shiftKey) {
                 class_list = el.querySelector("#grrrrid-container").classList;
                 class_list.toggle("grrrrid-container");
                 class_list.toggle("grrrrid-container-fluid");
+                return false;
               }
               if (e.keyCode === 27) {
                 $body.removeChild(el.querySelector(".grrrrid"));
