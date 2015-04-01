852ff45849f5221017d9c8a338e4a705e9add69d
vassilis <vas.terzo@gmail.com>
2015-04-01 15:49:41 +0300

    moved counter out of the main grid to avoid css conflicts

--- grid.js
+++ grid.js
@@ -4,7 +4,7 @@
     initGrrrridlet = function() {
       return (window.grrrridlet = function() {
         var el, $body, $style, $grrrrid, $grrrrid_container,
-            $grrrrid_counter, class_list, opacity, viewport, width, hide_counter;
+            $counter, $grrrrid_counter, class_list, opacity, viewport, width, hide_counter;
 
           el = document;
           $body = el.body;
@@ -12,16 +12,20 @@
             $body.removeChild(el.querySelector(".grrrrid"));
             $body.removeChild(el.querySelector("#grrrrid-style"));
           } else {
-            $grid = el.createElement("DIV");
+            $grid = el.createElement("div");
             $grid.setAttribute("class", "grrrrid");
-            $grid.innerHTML = '<span class="grrrrid-counter"></span><div id="grrrrid-container" class="grrrrid-container"><div class="grrrrid-row"><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div></div></div>';
+            $grid.innerHTML = '<div id="grrrrid-container" class="grrrrid-container"><div class="grrrrid-row"><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div></div></div>';
 
-            $style = el.createElement("STYLE");
+            $counter = el.createElement("span");
+            $counter.setAttribute("class", "grrrrid-counter");
+
+            $style = el.createElement("style");
             $style.setAttribute("id", "grrrrid-style");
             $style.textContent = ".grrrrid-counter{position:fixed;z-index:2147483647;top:10px;right:10px;font:1em/1 monospace;background:#FFF;padding:2px;color:#000;visibility:hidden;}.grrrrid *{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box}.grrrrid{position:fixed;top:0;left:0;z-index:2147483646;width:100%;height:100%;margin:0 auto;opacity:.5}.grrrrid div{height:100%}.grrrrid-container,.grrrrid-container-fluid{margin-right:auto;margin-left:auto;padding-left:15px;padding-right:15px}@media (min-width:768px){.grrrrid-container{width:750px}}@media (min-width:992px){.grrrrid-container{width:970px}}@media (min-width:1200px){.grrrrid-container{width:1170px}}.grrrrid-row{margin-left:-15px;margin-right:-15px}.grrrrid-col{float:left;width:8.33333333%;position:relative;min-height:1px;padding-left:15px;padding-right:15px}.grrrrid-col div{background:#FFA;height:100%}";
 
             $body.appendChild($style);
             $body.appendChild($grid);
+            $body.appendChild($counter);
 
             $grrrrid = el.querySelector(".grrrrid");
             $grrrrid_container = el.querySelector(".grrrrid-container");
