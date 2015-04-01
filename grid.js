dc3b41594b94c718fbfb07fd7b313e010a2585e0
vassilis <vas.terzo@gmail.com>
2015-04-01 15:36:04 +0300

    added width counter and cleaned up event listeners

--- grid.js
+++ grid.js
@@ -3,7 +3,9 @@
     var done, initGrrrridlet, script, v;
     initGrrrridlet = function() {
       return (window.grrrridlet = function() {
-        var el, $body, $style, $grrrrid, $grrrrid_container, class_list, opacity, viewport, width;
+        var el, $body, $style, $grrrrid, $grrrrid_container,
+            $grrrrid_counter, class_list, opacity, viewport, width, hide_counter;
+
           el = document;
           $body = el.body;
           if ($body.querySelector(".grrrrid") !== null) {
@@ -12,35 +14,54 @@
           } else {
             $grid = el.createElement("DIV");
             $grid.setAttribute("class", "grrrrid");
-            $grid.innerHTML = '<div id="grrrrid-container" class="grrrrid-container"><div class="grrrrid-row"><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div></div></div>';
+            $grid.innerHTML = '<span class="grrrrid-counter"></span><div id="grrrrid-container" class="grrrrid-container"><div class="grrrrid-row"><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div><div class="grrrrid-col"><div></div></div></div></div>';
 
             $style = el.createElement("STYLE");
             $style.setAttribute("id", "grrrrid-style");
-            $style.textContent = ".grrrrid *{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box}.grrrrid{position:fixed;top:0;left:0;z-index:2147483647;width:100%;height:100%;margin:0 auto;opacity:.5}.grrrrid div{height:100%}.grrrrid-container,.grrrrid-container-fluid{margin-right:auto;margin-left:auto;padding-left:15px;padding-right:15px}@media (min-width:768px){.grrrrid-container{width:750px}}@media (min-width:992px){.grrrrid-container{width:970px}}@media (min-width:1200px){.grrrrid-container{width:1170px}}.grrrrid-row{margin-left:-15px;margin-right:-15px}.grrrrid-col{float:left;width:8.33333333%;position:relative;min-height:1px;padding-left:15px;padding-right:15px}.grrrrid-col div{background:#FFA;height:100%}";
+            $style.textContent = ".grrrrid-counter{position:fixed;z-index:2147483647;top:10px;right:10px;font:1em/1 monospace;background:#FFF;padding:2px;}.grrrrid *{-webkit-box-sizing:border-box;-moz-box-sizing:border-box;box-sizing:border-box}.grrrrid{position:fixed;top:0;left:0;z-index:2147483646;width:100%;height:100%;margin:0 auto;opacity:.5}.grrrrid div{height:100%}.grrrrid-container,.grrrrid-container-fluid{margin-right:auto;margin-left:auto;padding-left:15px;padding-right:15px}@media (min-width:768px){.grrrrid-container{width:750px}}@media (min-width:992px){.grrrrid-container{width:970px}}@media (min-width:1200px){.grrrrid-container{width:1170px}}.grrrrid-row{margin-left:-15px;margin-right:-15px}.grrrrid-col{float:left;width:8.33333333%;position:relative;min-height:1px;padding-left:15px;padding-right:15px}.grrrrid-col div{background:#FFA;height:100%}";
 
             $body.appendChild($style);
             $body.appendChild($grid);
 
             $grrrrid = el.querySelector(".grrrrid");
             $grrrrid_container = el.querySelector(".grrrrid-container");
+            $grrrrid_counter = el.querySelector(".grrrrid-counter");
             $grrrrid.focus();
             window.onfocus = function() { $grrrrid.focus(); }
 
-            return el.addEventListener("keydown", function(e) {
+            el.addEventListener("keyup", onkeyup, true);
+            el.addEventListener("keydown", onkeydown, true);
+
+            function onkeyup(e){
+              hide_counter = setTimeout(function(){
+                $grrrrid_counter.style.visibility = "hidden";
+              }, 750)
+              return false;
+            }
+
+            function onkeydown(e){
               // console.log(e.keyCode);
               opacity = parseFloat(getComputedStyle($grrrrid)["opacity"]);
               viewport = parseFloat($body.clientWidth);
               width = parseFloat(getComputedStyle($grrrrid_container)["width"]);
 
+              clearTimeout(hide_counter);
+
               if (e.keyCode === 37 && e.shiftKey) { // left
                 if (width > 0) {
                   $grrrrid_container.style.width = width - 5 + "px";
+                  $grrrrid_counter.style.visibility = "visible";
+                  $grrrrid_counter.textContent = getComputedStyle($grrrrid_container)["width"];
                 }
+                return false;
               }
               if (e.keyCode === 39 && e.shiftKey) { // right
                 if (width < viewport) {
                   $grrrrid_container.style.width = width + 5 + "px";
+                  $grrrrid_counter.style.visibility = "visible";
+                  $grrrrid_counter.textContent = getComputedStyle($grrrrid_container)["width"];
                 }
+                return false;
               }
               if (e.keyCode === 38 && e.shiftKey) { // up
                 if (opacity < 1) {
@@ -55,6 +76,7 @@
                 return false;
               }
               if (e.keyCode === 70 && e.shiftKey) { // reset and toggle fluid
+                $grrrrid_counter.style.visibility = "hidden";
                 if ($grrrrid_container.getAttribute('style') != null) {
                   $grrrrid_container.removeAttribute('style');
                 } else {
@@ -64,11 +86,14 @@
                 }
                 return false;
               }
-              if (e.keyCode === 27) {
+              if (e.keyCode === 27) { // exit
                 $body.removeChild(el.querySelector(".grrrrid"));
                 $body.removeChild(el.querySelector("#grrrrid-style"));
+                el.removeEventListener("keyup", onkeyup, true);
+                el.removeEventListener("keydown", onkeydown, true);
+                return false;
               }
-            });
+            }
           }
       })();
     };
